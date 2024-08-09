a1f59186ee080e91b373a40f89a1ab504d5b27c6
jaehyeon2 <wol5971@gmail.com>
2024-08-09 23:46:54 +0900

    controller getUser

--- src/main/java/com/example/project/controller/DashboardController.java
+++ src/main/java/com/example/project/controller/DashboardController.java
@@ -1,17 +1,30 @@
 package com.example.project.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.project.beans.model.UserModel;
+import com.example.project.beans.model.ViewModel;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController extends BaseController{
 	
 	@GetMapping(value={"/", "", "/index"})
-	public String indexDashboard() throws Exception{
+	public String indexDashboard(HttpServletRequest request, HttpServletResponse response, HttpSession session, ViewModel viewModel, ModelMap map) throws Exception{
+		UserModel user = this.getUser(session);
 		
-		return "";
+		viewModel.setUser(user);
+		
+		map.addAttribute("model", viewModel);
+		
+		return "/dashboard/index";
 	}
 
 }
